79c0ddccf556b3bba206732bc8a0a64f4769e220
성윤희 <84892419+yunhee690@users.noreply.github.com>
2021-06-15 11:40:32 +0900

    Create HellGit.java

---
+++ 99.그림설명/src/HellGit.java
@@ -0,0 +1,18 @@
+public class HellGit {
+	private int member1;
+	private int member2;
+	public int getMember1() {
+		return member1;
+	}
+	public void setMember1(int member1) {
+		this.member1 = member1;
+	}
+	public int getMember2() {
+		return member2;
+	}
+	public void setMember2(int member2) {
+		this.member2 = member2;
+	}
+
+
+}
