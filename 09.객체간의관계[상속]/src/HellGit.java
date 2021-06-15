796e52ba86bf127e4e5e3ad9b2b31cb49c427f12
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-15 11:34:39 +0900

    .

--- 09.객체간의관계[상속]/src/HellGit.java
+++ 09.객체간의관계[상속]/src/HellGit.java
@@ -1,4 +1,19 @@
 
 public class HellGit {
+	private int member1;
+	private int member3;
+	public int getMember1() {
+		return member1;
+	}
+	public void setMember1(int member1) {
+		this.member1 = member1;
+	}
+	public int getMember3() {
+		return member3;
+	}
+	public void setMember3(int member3) {
+		this.member3 = member3;
+	}
+
 
 }
