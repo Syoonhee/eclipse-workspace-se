3ec298d8dba3233860dea0d0455c2b8c85c6780e
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-15 11:39:56 +0900

    ...

--- 99.그림설명/src/HellGit.java
+++ 99.그림설명/src/HellGit.java
@@ -1,20 +1,17 @@
-
 public class HellGit {
 	private int member1;
-	private int member2;
-  
+	private int member3;
 	public int getMember1() {
 		return member1;
 	}
-	
-  public void setMember1(int member1) {
+	public void setMember1(int member1) {
 		this.member1 = member1;
 	}
-	public int getMember2() {
-		return member2;
+	public int getMember3() {
+		return member3;
 	}
-	public void setMember2(int member2) {
-		this.member2 = member2;
+	public void setMember3(int member3) {
+		this.member3 = member3;
 	}
 
 
