5e01d4afc1e3391a0e5ee5e9ee07beb2aa539f45
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-15 14:18:33 +0900

    ....

--- 09.객체간의관계[상속]/src/AcademyMemberMain.java
+++ 09.객체간의관계[상속]/src/AcademyMemberMain.java
@@ -3,8 +3,39 @@ public class AcademyMemberMain {
 
 	public static void main(String[] args) {
 		//AcademyMember m1 = new AcademyMember(0, "누구게");
-		
+		AcademyStudent st1 = new AcademyStudent(1, "KIM", "LINUX");
+		AcademyStudent st2 = new AcademyStudent(2, "HIM", "JAVA");
+		AcademyStudent st3 = new AcademyStudent(3, "JIM", "DESIGN");
 
+		AcademyGansa gs1 = new AcademyGangsa(4, "LEE", "ANDROID"); 
+		AcademyGansa gs2 = new AcademyGangsa(5, "SIM", "IOS"); 
+
+		AcademyStaff sf1 = new AcademyStaff(6, "WIM", "MARKETING"); 
+		AcademyStaff sf2 = new AcademyStaff(7, "WIM", "ACCOUNTING"); 
+		
+		/*st1.print();
+		st2.print();
+		st3.print();
+		gs1.print();
+		gs2.print();
+		sf1.print();
+		sf1.print();
+		*/
+		
+		
+		System.out.println("---------Student-----------");
+		st1.studentPrint();
+		st2.studentPrint();
+		st3.studentPrint();
+
+		System.out.println("-------Gangsa-------------");
+		gs1.gangsaPrint();
+		gs2.gangsaPrint();
+		
+		
+		System.out.println("----------Staff-----------");
+		sf1.staffPrint();
+		sf2.staffPrint();
 	}
 
 }
