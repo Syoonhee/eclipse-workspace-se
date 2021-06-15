93a45056079e8b9272358dbf6ee6b68311b0d7a5
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-15 16:13:18 +0900

    .

--- 11.객체간의관계[상속]-생성자/src/ParentChildMain.java
+++ 11.객체간의관계[상속]-생성자/src/ParentChildMain.java
@@ -1,26 +1,35 @@
-class Parent{
+class Parent extends Object{
 	public int member1;
 	public int member2;
-
+	public Parent() {
+		System.out.println("Parent()디폴트생성자");
+	}
 	public void method1() {
 		System.out.println("Parent.method1()");
 	}
 	public void method2() {
 		System.out.println("Parent.method2()");
+		
 	}
 }
-
 class Child extends Parent{
 	public int member3;
+	public Child() {
+		System.out.println("Child()디폴트생성자");
+	}
 	public void method3() {
 		System.out.println("Child.method3()");
 	}
 }
 
 
+
 public class ParentChildMain {
 
 	public static void main(String[] args) {
+		System.out.println("--------------------");
+		Child c1=new Child();
+		System.out.println("--------------------");
 		
 	}
 
