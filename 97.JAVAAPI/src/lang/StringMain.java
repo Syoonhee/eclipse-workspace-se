8b90eb90a077135a3e5e961c3b2282ecb447c26d
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-16 16:01:33 +0900

    ...

--- 97.JAVAAPI/src/lang/StringMain.java
+++ 97.JAVAAPI/src/lang/StringMain.java
@@ -10,6 +10,19 @@ public class StringMain {
 		
 		String name1 = new String("KIM");
 		String name2 = new String("KIM");
+		if(name1==name2) {
+			System.out.println("name1과 name2의 주소가 같다");
+		} else {
+			System.out.println("name1과 name2의 주소가 다르다");
+		}
+		
+		if(name1.equals(name2)) {
+			System.out.println("name1의 문자열의 값과 name2 문자열의 값이 동일하다");
+		}else {
+			System.out.println("name1의 문자열의 값과 name2 문자열의 값이 동일하지 않다");
+
+		}
+		
 		
 		/*
 		 * String 객체의 암시적 생성(생성자 호출 안함)
