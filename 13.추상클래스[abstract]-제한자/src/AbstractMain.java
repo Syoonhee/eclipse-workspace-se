87f20c422701bd942d245ef504679f900183fda4
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-17 15:23:44 +0900

    ...

--- 13.추상클래스[abstract]-제한자/src/AbstractMain.java
+++ 13.추상클래스[abstract]-제한자/src/AbstractMain.java
@@ -8,6 +8,23 @@ public class AbstractMain {
 		AbstractClassChild acc1 = new AbstractClassChild();
 		acc1.method1();
 		acc1.method2();
+		
+		AbstractClass acc2 = acc1; //acc1과 acc2가 같은 객체 가리킴
+		acc2.method1();
+		acc2.method2();
+		
+		AbstractClass acc3 = new AbstractClassChild(); //상위 클래스에 넣는 이유: 유지보수, 확장성
+		acc3.method1();
+		acc3.method2();
+		System.out.println("-----------array---------------");
+		AbstractClass[] accArray = new AbstractClass[3]; 
+		accArray[0] = new AbstractClassChild();
+		accArray[1] = new AbstractClassChild();
+		accArray[2] = new AbstractClassChild();
+		for (int i = 0; i < accArray.length; i++) {
+			accArray[i].method2();
+		}
+ 		
 	}
 
 }
