43978b7665064ef808601e896d510b534f35f39d
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-18 11:25:11 +0900

    ...

--- 14.인터페이스[interface]/src/InterfaceABImpl.java
+++ 14.인터페이스[interface]/src/InterfaceABImpl.java
@@ -3,26 +3,22 @@ public class InterfaceABImpl extends Object implements InterfaceA,InterfaceB{
 
 	@Override
 	public void method3() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("InterfaceB.method3()의 구현(재정의)");
 	}
 
 	@Override
 	public void method4() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("InterfaceB.method4()의 구현(재정의)");
 	}
 
 	@Override
 	public void method1() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("InterfaceA.method1()의 구현(재정의)");		
 	}
 
 	@Override
 	public void method2() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("InterfaceA.method2()의 구현(재정의)");		
 	}
 
 }
