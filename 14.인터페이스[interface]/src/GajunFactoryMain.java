97d670b3395c15587bd8a7e05dd8d4f9bbe769ed
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-18 14:40:26 +0900

    ...

--- 14.인터페이스[interface]/src/GajunFactoryMain.java
+++ 14.인터페이스[interface]/src/GajunFactoryMain.java
@@ -18,11 +18,12 @@ public class GajunFactoryMain {
 		gajunArray[3] = audio1;
 		gajunArray[4] = audio2;
  		System.out.println("---------------가전제품 검사소(볼륨,OnOff)-----------------");
-		/*
-		 * 난 절대로 자식 클래스 타입을 사용 안 할래요
-		 * 난 부모타입[GajunVolume,GajunOnOff]만 사용할래요
-		 * 그래야 가전제품 검사 프로그램을 한 번 만들어서 변경없이 계속 사용할 수 있으니까요
-		 */
+ 		/*
+ 		* 난 절대로 자식 클래스 타입을 사용 안 할래요
+ 		* 난 부모타입[GajunVolume,GajunOnOff]만 사용할래요
+ 		* 그래야 가전제품 검사 프로그램을 한 번 만들어서 변경없이 (유지보수, 확장성) 계속 사용할 수 있으니까요 
+ 		*  유지보수시 많이 바꾸지 않아도 된다. 
+ 		*/
  		GajunOnOff[] recvGajunArray = gajunArray;
  		for (int i = 0; i < recvGajunArray.length; i++) {
 			recvGajunArray[i].on();
