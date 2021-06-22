834ef1ffe47f2a8663905e452e9724dbabda6a12
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-22 11:51:24 +0900

    ...

---
+++ 18.예외[Exception]/src/FirstSecondExceptionThrowMain.java
@@ -0,0 +1,13 @@
+
+public class FirstSecondExceptionThrowMain {
+
+	public static void main(String[] args) {
+		System.out.println("1. main 흐름 start");
+		First first = new First();
+		System.out.println("2. main 흐름 first.method1() 호출전");
+		first.method1();
+		System.out.println("2. main 흐름 first.method1() 호출후");
+		return;
+	}
+
+}
--- 18.예외[Exception]/src/Second.java
+++ 18.예외[Exception]/src/Second.java
@@ -1,4 +1,21 @@
 
 public class Second {
-
+	public void method2() {
+		System.out.println("\t\t Second.method2() 실행");
+		String str = null;
+		int length = str.length();
+		System.out.println("length:" + length);
+		
+		/************case1***************/
+		/*
+		 * 1. JVM 예외상황을  감지하고 예외 객체(NullpointerException) 생성
+		 * 2. JVM은 생성한 예외 객체를 반환 방향으로 던진다. (throw)
+		 * 3. 실행 중인 쓰레드는 실행을 멈추고 호출한 곳으로 이동한다
+		 * 4. JVM은 던져진 에외 객체의 메세지를 출력한 후 쓰레드 종료(shut down)
+		 */
+		
+		
+		System.out.println("\t\t Second.method2() 반환");
+		return;
+	}
 }
