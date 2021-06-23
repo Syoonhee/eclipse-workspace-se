ec64873b17870e2a1bd9d96308acc4bf562a5733
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-23 14:52:06 +0900

    ...

---
+++ 19.컬렉션[Collection]/src/ArrayListMain.java
@@ -0,0 +1,11 @@
+
+public class ArrayListMain {
+
+	public static void main(String[] args) {
+		System.out.println("-----------Object[]-------------");
+		Object[] anyTypeArray = new Object[5];
+		System.out.println(anyTypeArray);
+		
+	}
+
+}
