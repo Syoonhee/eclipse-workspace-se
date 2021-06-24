3779ed63fad24d5c17414b48a459bfd77cf23f89
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-24 13:12:11 +0900

    ...

---
+++ 19.컬렉션[Collection]/src/HashSetMain.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+public class HashSetMain {
+
+	public static void main(String[] args) {
+		HashSet accountSet = new HashSet();
+		System.out.println("# set size:" + accountSet.size());
+		
+	
+	
+	
+	
+	
+	}
+
+}
