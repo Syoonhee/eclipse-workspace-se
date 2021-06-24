158b639bf829fdcab5890fb56b24fe52ca0993d0
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-24 13:12:41 +0900

    ...

--- 19.컬렉션[Collection]/src/HashSetMain.java
+++ 19.컬렉션[Collection]/src/HashSetMain.java
@@ -6,6 +6,11 @@ public class HashSetMain {
 		HashSet accountSet = new HashSet();
 		System.out.println("# set size:" + accountSet.size());
 		
+		Account acc1=new Account(1111, "BING",33000,0.5);
+		Account acc2=new Account(2222, "KING",23000,0.1);
+		Account acc3=new Account(3333, "KING",89000,0.2);
+		Account acc4=new Account(4444, "YONG",12000,0.5);
+		Account acc5=new Account(5555, "SANG",99000,0.8);
 	
 	
 	
