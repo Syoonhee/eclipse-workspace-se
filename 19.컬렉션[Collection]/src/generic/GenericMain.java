4f9d61c106535537c9aadd00000140d4934a95c0
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-25 10:50:54 +0900

    .

--- 19.컬렉션[Collection]/src/generic/GenericMain.java
+++ 19.컬렉션[Collection]/src/generic/GenericMain.java
@@ -5,8 +5,27 @@ public class GenericMain {
 	public static void main(String[] args) {
 		System.out.println("*****************NoGeneric********************");
 		NoGenericClass noGenericClass = new NoGenericClass();
-		noGenericClass.setMember(new String("멤버"));
-		noGenericClass.setMember(new Integer(123)); //상위캐스팅
+		
+		//noGenericClass.setMember(new String("멤버"));
+		//noGenericClass.setMember(new Integer(123)); //상위캐스팅
+		//noGenericClass.setMember(new Account());
+		//noGenericClass.setMember(new Car());
+		noGenericClass.setMember(new Account(1111,"FIRST",10000,1.0));
+		Account getAccount = (Account) noGenericClass.getMember();
+		getAccount.print();
+		System.out.println("*****************Generic********************");
+		GenericClass<Account> genericClass = new GenericClass<Account>();
+		genericClass.setMember(new Account(1111, "FIRST", 10000, 1.0));
+		getAccount = genericClass.getMember();
+		GenericClass<Car> genericClass2 = new GenericClass<Car>();
+		genericClass2.setMember(new Car("1234", 12, 0,0 ));
+		Car getCar = genericClass2.getMember();
+		getCar.print();
+		
+		
+		
+		
+		
 		
 	}
 
