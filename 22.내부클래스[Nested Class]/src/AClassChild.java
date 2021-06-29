8fde0baf67c02b539996134ce9ace726e3c96aef
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-29 13:31:14 +0900

    ....

---
+++ 22.내부클래스[Nested Class]/src/AClassChild.java
@@ -0,0 +1,8 @@
+public class AClassChild 
+	extends AClass{
+	@Override
+	public void methodA() {
+		System.out.println(
+				"AClassChild에서 AClass.methodA()재정의");
+	}
+}
--- 22.내부클래스[Nested Class]/src/CInterfac.java
+++
@@ -1,4 +0,0 @@
-
-public interface CInterfac {
-	public void methodC();
-}
