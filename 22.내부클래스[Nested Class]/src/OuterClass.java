5fcaf8387a7c8f08e558df97ba8b7095dd81bf25
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-29 09:39:30 +0900

    ...

--- 22.내부클래스[Nested Class]/src/OuterClass.java
+++ 22.내부클래스[Nested Class]/src/OuterClass.java
@@ -10,10 +10,29 @@ public class OuterClass {
 	public void outer_member_method() {
 		System.out.println("OuterClass.outer_member_method()");
 	}
+	
+	/*********외부클래스에서 내부클래스의 사용**********/
+	public void outer_inner_class_use() { 
+		/*
+		 * 1.InnerClass객체생성
+		 */
+		//OuterClass.InnerClass innerClassObject = this.new InnerClass(); 이너클래스는 단독으로 존재x (앞에 OuterClass 생략 가능) ,this는 왜..?(모르면 걍 넘어감ㅋ)
+		InnerClass innerClassObject = new InnerClass();
+		/*
+		 * 2.InnerClass객체멤버사용
+		 */
+		innerClassObject.inner_member_field = 99999;
+		innerClassObject.inner_member_method();
+		
+	}
+	
+	
+	
 	/*
 	 * 인스턴스 멤버 클래스[Nested class, Inner Class, 내부클래스]
 	 */
-	public class InnerClass{
+	/**************inner class******************/
+	public class InnerClass{ //아우터클래스가 생성되어야 의미가 있음 (사용도 아우터 클래스 안에서)
 		public int inner_member_field;
 		public void inner_member_method() {
 			System.out.println("InnerClass.inner_member_method()");
@@ -28,11 +47,13 @@ public class OuterClass {
 			//InnerClass.this.inner_member_field = 9090;
 			//OuterClass.this.outer_member_field = 7878;
 			outer_member_field = 7878;
+			//OuterClass.this.outer_member_method();
 			outer_member_method();
 		}
 		
 		
-	}
+	}//inner class end
+	/********************************/
 	
 	
-}
+} //end outer class
