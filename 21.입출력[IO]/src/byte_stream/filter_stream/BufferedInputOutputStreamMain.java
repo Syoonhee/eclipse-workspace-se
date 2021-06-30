d1391e3b81d7af033f729e6f5f611ef3c78b4665
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-06-30 10:05:29 +0900

    ...

--- 21.입출력[IO]/src/byte_stream/filter_stream/BufferedInputOutputStreamMain.java
+++ 21.입출력[IO]/src/byte_stream/filter_stream/BufferedInputOutputStreamMain.java
@@ -4,19 +4,21 @@ import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 
 public class BufferedInputOutputStreamMain {
-		public static void main(String[] args) throws Exception{
-		FileOutputStream fos = new FileOutputStream("bufferedOut.dat");
-		BufferedOutputStream bos = new BufferedOutputStream(fos);
-		bos.write(0b000000111010);
+	public static void main(String[] args) throws Exception{
+		
+		FileOutputStream fos=new FileOutputStream("bufferedOut.dat");
+		BufferedOutputStream bos=new BufferedOutputStream(fos);
+		bos.write(0b0000001110010);
 		bos.write(89);
 		bos.write(23);
-		for(int i=0; i<256; i++) {
+		for(int i=0;i<256;i++) {
 			bos.write(i);
 		}
 		bos.close();
 		System.out.println("BufferedOutputStream.write --> bufferedOut.dat ");
-
 		
-		}
+		
+		
+		
+	}
 }
-
