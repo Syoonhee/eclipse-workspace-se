5daf799c329bb7c4fc7a52d59a51f999eed71884
s_yun <s_yun@DESKTOP-LJRPR1U>
2021-07-13 17:06:35 +0900

    ...

--- 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
+++ 23.JDBC[Java DataBase Connectivity]/src/basic/JDBCFlowMain.java
@@ -8,8 +8,8 @@ public class JDBCFlowMain {
 	public static void main(String[] args) throws Exception {
 		String driverClass = "oracle.jdbc.OracleDriver";
 		String url = "jdbc:oracle:thin:@182.237.126.19:1521:xe";
-		String user = "javadeveloper0";
-		String password = "javadeveloper0";
+		String user = "javadeveloper20";
+		String password = "javadeveloper20";
 		
 		
 		/*
